d4864bb64df7ca3c42375731b3924065c878f2a7
hoffs <stephanie.nhoffman7@gmail.com>
2019-09-15 13:17:59 -0700

    working to generate random target score

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+//VARIABLES
+
+var targetNumber = 0;
+
+function getRandomTargetNumber(targetNumber){
+    return targetNumber = Math.random * (min - max + 1);
+        min = 19;
+        max = 120;
+}
+    
+
+// var counter = 0;
+
+$("#target-score-to-guess").text(targetNumber);
+
+// var randomNumberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+
+
+// $("#yellow-crystal").on(click, function() {
+// }
+
+// game begins with target number 
+
+// each crystal contains a random, hidden number
+
+// player clicks on a crystal option
+    // then Your Score shows that amount
+
+// player clicks on another crystal or smae crystal
+    // Your Score amount increases by that amount
+
+// player continues to click on crystal options until they get to target number
+
+// if player matches the target number, their Win goes up by 1, and then new random target number is generated (repeat process above)
+
+// if player goes over the target number, then Loss goes up by 1 and a new random target number is generated (repeat process above)
+
